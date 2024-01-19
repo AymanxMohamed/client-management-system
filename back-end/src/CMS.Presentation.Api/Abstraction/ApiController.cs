fa7ed1b7f25497acc9a4c0c145026f77e0648993
Ayman Kheireldeen <aymanmido067@gmail.com>
2024-01-19 17:40:53 +0200

    Adding Presentation Configurations

---
+++ back-end/src/CMS.Presentation.Api/Abstraction/ApiController.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CMS.Presentation.Api.Abstraction;
+
+[Route("api/[controller]")]
+public class ApiController : ControllerBase
+{
+    protected readonly ISender Sender;
+    public ApiController(ISender sender) => Sender = sender;
+}
---
+++ back-end/src/CMS.Presentation.Api/Extensions/ApplicationServicesExtensions.cs
@@ -0,0 +1,29 @@
+﻿using CMS.Application;
+using CMS.Infrastructure;
+
+namespace CMS.Presentation.Api.Extensions;
+
+public static class ApplicationServicesExtensions
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services,
+        IConfiguration configuration)
+    {
+
+        services
+            .AddPresentation()
+            .AddApplication()
+            .AddInfrastructure(configuration)
+            .AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy
+                        .AllowAnyHeader()
+                        .AllowAnyMethod()
+                        .AllowAnyOrigin();
+                });
+            });
+        
+        return services;
+    }
+}
